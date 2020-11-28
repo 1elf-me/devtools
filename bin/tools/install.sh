587c1c7c353377893679e70ea25f8f411e8bde5e
Maurice Preuß <hello@1elf.me>
2020-11-28 09:47:00 +0100

    Updated install.sh

--- bin/tools/install.sh
+++ bin/tools/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+apt-get install -y git
+
+mkdir -p /opt/devtools
+
+git clone https://github.com/1elf-me/devtools.git /opt/devtools
+
 chmod +x /opt/devtools/devtools
+
+ln -s /opt/devtools/devtools /usr/share/devtools
