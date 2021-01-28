16da7da7f3c68f5dddb0590284465e27dae10065
1elf <63093594+1elf-me@users.noreply.github.com>
2021-01-28 13:10:31 +0100

    update autoupdate

--- bin/self/autoupdate.sh
+++ bin/self/autoupdate.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-# added auto update flag
-touch "$DEVTOOLS_DIRECTORY/.autoupdate"
+# set config variables
+export status=${3}
 
+# shellcheck disable=SC2016
+export try_files='$uri $uri/ =404' # export try_files='$uri $uri/ /index.php?$query_string'
+
+# check if domain name is set
+if [ -z "$status" ]; then
+  echo "Error: status not entered! Options: enable, disable"
+  exit
+fi
+
+# set autoupdate by status
+if [ "$status" = "enable" ]; then
+  touch "$DEVTOOLS_DIRECTORY/.autoupdate"
+else if [ "$status" = "disable" ]; then
+  rm -f "$DEVTOOLS_DIRECTORY/.autoupdate"  
+else
+  echo "Status not available"
+  exit 1
+fi
+
+# test file
 if [ -f "$DEVTOOLS_DIRECTORY/.autoupdate" ]; then
   echo "autoupdate has been successfully enabled!"
 else
-  echo "$DEVTOOLS_DIRECTORY/.autoupdate can't be created"
+  echo "autoupdate has been disabled!"
 fi
